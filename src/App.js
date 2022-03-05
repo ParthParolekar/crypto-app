99eed23efc30c8b45c1021346e3f4f74589c5e67
ParthParolekar <parolekarparth@gmail.com>
2022-03-05 14:57:50 +0530

    add: routes and reset styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Coinpage from "./pages/Coinpage";
+import Homepage from "./pages/Homepage";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div>
+        Crypto App
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coin/:asds" element={<Coinpage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/Coinpage.js
@@ -0,0 +1,3 @@
+export default function Coinpage() {
+  return <h1>Coin page</h1>;
+}
